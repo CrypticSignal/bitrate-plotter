422c4380dbacd38ac6ca099cd049c1f5e42178a9
CrypticSignal <hshafiq@hotmail.co.uk>
2021-02-14 18:19:13 +0000

    Misc.

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # Bitrate Variation Plotter
-A command line program that plots the graph showing the variation of the bitrate throughout the specified audio/video file.
+A command line program that plots a graph showing the variation of the bitrate throughout the specified audio/video file.
+
+You can find an example graph below:
+
+![Example Graph](https://github.com/CrypticSignal/bitrate-variation-plotter/blob/main/Example%20Graph.png)
+
+*The above graph is for a Variable Bitrate (VBR) MP3 file, encoded using the -V0 setting of the [LAME](https://lame.sourceforge.io/) encoder.*
+
+In addition to this, the data used to plot the graph is saved in a file named `Raw Data.txt`. The data is in the format `timestamp --> bitrate`. Here is a sample:
+```
+1.008 --> 223 kbps
+2.016 --> 257 kbps
+3.0 --> 239 kbps
+4.008 --> 250 kbps
+5.016 --> 263 kbps
+6.0 --> 253 kbps
+7.008 --> 264 kbps
+8.016 --> 267 kbps
+9.0 --> 260 kbps
+10.008 --> 266 kbps
+```
+# Usage
+Using this program is very simple. As an example, to analyse a file named video.mp4 in the current directory, enter `python main.py -f video.mp4`
+
+The only argument that is required is the path of the file that you wish to analyse. All other arguments are optional. You can find the output of `python main.py -h` below:
+```
+usage: main.py [-h] -f FILE_PATH [-o OUTPUT_FOLDER] [-s STREAM_SPECIFIER] [-t GRAPH_TITLE]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -f FILE_PATH, --file-path FILE_PATH
+                        Enter the path of the file that you want to analyse.
+                        If the path contains a space, it must be surrounded in double quotes.
+                        Example: -f "C:/Users/H/Desktop/my file.mp4"
+  -o OUTPUT_FOLDER, --output-folder OUTPUT_FOLDER
+                        Change the name of the folder where the data will be saved.
+                        If the desired folder name contains a space, it must be surrounded in double quotes.
+                        Default folder name: (<file being analysed>).
+                        i.e. if the file being analysed is video.mp4, the output folder will be named (video.mp4)
+                        Example: -o "my folder"
+  -s STREAM_SPECIFIER, --stream-specifier STREAM_SPECIFIER
+                        Use FFmpeg stream specifier syntax to specify the audio/video stream that you want to analyse.
+                        By default, the graph is based on the first audio stream.
+                        Stream index starts at 0, therefore, as an example, to target the 2nd audio stream, enter -s a:1
+  -t GRAPH_TITLE, --graph-title GRAPH_TITLE
+                        Specify a title for the output graph.
+                        By default, the title of the graph will simply be the name of the file that was analysed.
+```
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from argparse import ArgumentParser, RawTextHelpFormatter
 from pathlib import Path
+import os
 import subprocess
 import matplotlib.pyplot as plt
 
@@ -9,40 +10,47 @@ parser.add_argument(
     '-f', '--file-path', 
     type=str, 
     required=True,
-    help='Enter the path of the file that you want to analyse. A relative or absolute path can be specified. '
+    help='Enter the path of the file that you want to analyse.\n'
          'If the path contains a space, it must be surrounded in double quotes.\n'
          'Example: -f "C:/Users/H/Desktop/my file.mp4"'
 )
 parser.add_argument(
-    '-o', '--output-graph-filename', 
+    '-o', '--output-folder', 
     type=str, 
-    help='Specify a filename (excluding the .png part) for the graph.'
-         'By default, the name of the file being analysed will be used, i.e. if the file is named audio.mp3, '
-         'the graph will be saved as audio.png\n'
-         'Example: -o mygraph (this will save the graph as mygraph.png)'
+    help='Change the name of the folder where the data will be saved.\n'
+         'If the desired folder name contains a space, it must be surrounded in double quotes.\n'
+         'Default folder name: (<file being analysed>).\n'
+         'i.e. if the file being analysed is video.mp4, the output folder will be named (video.mp4)\n'
+         'Example: -o "my folder"'
 )
 parser.add_argument(
     '-s', '--stream-specifier', 
     type=str, 
     default='a:0',
-    help='Use FFmpeg stream specifier syntax to specify the stream that you want to target.\n'
+    help='Use FFmpeg stream specifier syntax to specify the audio/video stream that you want to analyse.\n'
          'By default, the graph is based on the first audio stream.\n'
          'Stream index starts at 0, therefore, as an example, to target the 2nd audio stream, enter -s a:1'
 )
 parser.add_argument(
     '-t', '--graph-title', 
     type=str,
-    help='Specify a title for the output graph. '
-         'By default, the title of the graph will simply be the filename of the audio file.'
+    help='Specify a title for the output graph.\n'
+         'By default, the title of the graph will simply be the name of the file that was analysed.'
 )
 
 args = parser.parse_args()
 filename = Path(args.file_path).name
+output_folder = f'({filename})'
 filename_without_ext = Path(args.file_path).stem
+
+if args.output_folder:
+    output_folder = args.output_folder
+    
+os.makedirs(output_folder, exist_ok=True)
 
 
 def update_txt_file(text, mode='a'):
-    with open(f'{filename_without_ext}.txt', mode) as f:
+    with open(f'{output_folder}/Raw Data.txt', mode) as f:
         f.write(text)
 
 
@@ -89,19 +97,18 @@ for line in output:
             size_so_far = 0
         else:
             size_so_far += (int(line[9:]) * 8) / 1000
-            # Multiplied by 8 to convert bytes to bits, then divided by 1000 to converts to kbps
+            # Multiplied by 8 to convert bytes to bits, then divided by 1000 to convert to kbps
 
 min = round(min(size_data), 1)
 max = round(max(size_data), 1)
 update_txt_file(f'\nMin Bitrate: {min} kbps\nMax Bitrate: {max} kbps')
 
-graph_filename = f'{filename_without_ext}.png' if not args.output_graph_filename else args.output_graph_filename
 graph_title = filename if not args.graph_title else args.graph_title
 
 print('Creating the graph...')
-plt.plot(time_data, size_data)
 plt.suptitle(graph_title)
 plt.xlabel('Time (s)')
 plt.ylabel('Bitrate (kbps)')
-plt.savefig(graph_filename)
-print('Done!')
+plt.plot(time_data, size_data)
+plt.savefig(os.path.join(output_folder, f'{filename_without_ext}.png'))
+print(f'Done! Check out the "{output_folder}" folder.')

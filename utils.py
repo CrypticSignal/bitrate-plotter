536a6a10b98cbb886ebe47a78141465ff0c571c3
CrypticSignal <hshafiq@hotmail.co.uk>
2021-02-18 23:31:08 +0000

    Return number_of_frames

--- utils.py
+++ utils.py
@@ -7,13 +7,14 @@ def calc_number_of_frames(file_path, stream_specifier, file_duration):
         'ffprobe', '-v', 'error', '-threads', str(os.cpu_count()),
         '-select_streams', stream_specifier, 
         '-show_entries', 'stream=r_frame_rate', '-of', 'default=noprint_wrappers=1:nokey=1',
-        args.file_path
+        file_path
     ]
     process = subprocess.run(cmd, capture_output=True)
     output = process.stdout.decode('utf-8')
     numerator, denominator = output.split('/')
     framerate = int(numerator) / int(denominator)
     number_of_frames = framerate * file_duration
+    return number_of_frames
 
 
 def get_file_duration(file_path, stream_specifier):

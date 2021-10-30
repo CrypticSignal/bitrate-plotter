259c6cd6206f14a54f9283f5c7381897fd75f7ef
CrypticSignal <hshafiq@hotmail.co.uk>
2021-10-30 15:21:31 +0100

    Use decoding timestamps instead of presentation timestamps + show bitrate in Mbps instead of Kbps.

--- ffprobe_output_parser.py
+++ ffprobe_output_parser.py
@@ -8,42 +8,50 @@ from utils import clear_current_line_in_terminal, write_to_txt_file
 def get_bitrate_every_second(process, raw_data_filename, file_duration):
     x_axis_values = []
     bitrate_every_second = []
-    kilobits_this_second = 0
+    megabits_this_second = 0
     # Initially, get the bitrate for the first second.
     # After every second, this value is incremented by 1 so we can get the bitrate for the 2nd second, 3rd second, etc.
     time_to_check = 1
-    # We will create a dictionary where the pts_time is the value and the frame size is the value.
-    pts_time_and_size = {}
+    # Initialise a dictionary where the decoding timestamps (DTS) will be the keys and the packet sizes will be the values.
+    dts_times_and_packet_sizes = {}
 
     for line in io.TextIOWrapper(process.stdout, encoding="utf-8"):
-        pts_time, size = line.strip().split(',')
-        pts_time = float(pts_time[9:])
-        frame_size = (int(size[5:]) * 8) / 1000
+        ## ffprobe will return the time in ms and the size in bytes.
+        dts_time, packet_size = line.strip().split(",")
+        packet_size = int(packet_size)
+        # Convert to megabits.
+        packet_size = (packet_size * 8) / 1000_000
 
-        pts_time_and_size[pts_time] = frame_size
+        try:
+            float(dts_time)
+        except Exception:
+            pass
+        else:
+            dts_times_and_packet_sizes[float(dts_time)] = packet_size
+            percentage_complete = round(((float(dts_time) / file_duration) * 100.0), 1)
+            print(f"Progress: {percentage_complete}%", end="\r")
 
-        percentage_complete = round(((pts_time / file_duration) * 100.0), 1)
-        print(f'Progress: {percentage_complete}%', end='\r')
-    
-    clear_current_line_in_terminal() # Clears the progress and ETA.
-    print('Done!')
+    clear_current_line_in_terminal()  # Clears the progress and ETA.
+    print("Done!")
     # Create a new dictionary where the entries are ordered by timestamp value (ascending order).
-    ordered_dict = dict(sorted(pts_time_and_size.items()))
-    print('Calculating the bitrates...')
+    ordered_dict = dict(sorted(dts_times_and_packet_sizes.items()))
+    print("Calculating the bitrates...")
 
-    for pts_time, frame_size in ordered_dict.items():
-        if pts_time >= time_to_check:
-            x_axis_values.append(pts_time)
-            bitrate_every_second.append(kilobits_this_second)
+    for dts_time, packet_size in ordered_dict.items():
+        if dts_time >= time_to_check:
+            x_axis_values.append(dts_time)
+            bitrate_every_second.append(megabits_this_second)
 
-            percentage_complete = round(100.0 * (pts_time / file_duration), 1)
-            #print(f'Progress: {percentage_complete}%', end='\r')
-            write_to_txt_file(raw_data_filename, f'Timestamp: {pts_time} --> {round(kilobits_this_second)} kbps\n')
-        
-            kilobits_this_second = frame_size
+            percentage_complete = round(100.0 * (dts_time / file_duration), 1)
+            print(f'Progress: {percentage_complete}%', end='\r')
+            write_to_txt_file(
+                raw_data_filename, f"Timestamp: {dts_time} --> {round(megabits_this_second)} Mbps\n"
+            )
+
+            megabits_this_second = packet_size
             time_to_check += 1
         else:
-            kilobits_this_second += frame_size
+            megabits_this_second += packet_size
 
     clear_current_line_in_terminal()
     return x_axis_values, bitrate_every_second
@@ -60,32 +68,41 @@ def get_gop_bitrates(process, number_of_frames, data_output_path):
     for line in io.TextIOWrapper(process.stdout):
         frame_count += 1
         write_to_txt_file(data_output_path, line)
-      
-        key_frame, pkt_pts_time, pkt_size = line.strip().split(',')
-        pkt_size = (int(pkt_size[9:]) * 8) / 1000
 
-        if key_frame[10:] == '1':
-            keyframe_count += 1
-            pkt_pts_time = float(pkt_pts_time[13:])
+        key_frame, pkt_dts_time, pkt_size = line.strip().split(",")
+        # Convert from bytes to megabits.
+        pkt_size = (int(pkt_size) * 8) / 1000_000
 
-            if keyframe_count == 1:
-                gop_length = 1
-                gop_size += pkt_size
-                previous_pkt_pts_time = pkt_pts_time
-            else:
-                gop_end_times.append(pkt_pts_time)
-                gop_duration = pkt_pts_time - previous_pkt_pts_time
-                gop_bitrates.append(gop_size / gop_duration)
-
-                previous_pkt_pts_time = pkt_pts_time
-                gop_size = pkt_size
-                # We've reached a new keyframe, set gop_length to 1.
-                gop_length = 1      
+        try:
+            float(pkt_dts_time)
+        except Exception:
+            pass
         else:
-            gop_length += 1
-            gop_size += pkt_size
+            pkt_dts_time = float(pkt_dts_time)
+            # key_frame=1 (with H.264, this is an IDR frame).
+            if key_frame == "1":
+                keyframe_count += 1
 
-        percentage_progress = round((frame_count / number_of_frames) * 100, 1)
-        print(f'Progress: {percentage_progress}%', end='\r')
-            
+                if keyframe_count == 1:
+                    gop_length = 1
+                    gop_size += pkt_size
+                    previous_pkt_dts_time = pkt_dts_time
+                else:
+                    gop_end_times.append(pkt_dts_time)
+                    gop_duration = pkt_dts_time - previous_pkt_dts_time
+                    gop_bitrates.append(gop_size / gop_duration)
+
+                    previous_pkt_dts_time = pkt_dts_time
+                    gop_size = pkt_size
+                    # We've reached a new keyframe, set gop_length to 1.
+                    gop_length = 1
+
+            # key_frame=0
+            else:
+                gop_length += 1
+                gop_size += pkt_size
+
+            percentage_progress = round((frame_count / number_of_frames) * 100, 1)
+            print(f"Progress: {percentage_progress}%", end="\r")
+
     return gop_end_times, gop_bitrates
--- main.py
+++ main.py
@@ -12,46 +12,51 @@ from utils import calc_number_of_frames, get_file_duration, write_to_txt_file
 
 parser = ArgumentParser()
 parser.add_argument(
-    '-f', '--file-path', 
-    type=str, 
-    required=True,
-    help='Enter the path of the file that you want to analyse. '
-         'If the path contains a space, it must be surrounded in double quotes. '
-         'Example: -f "C:/Users/H/Desktop/my file.mp4"'
-)
-parser.add_argument(
-    '-g', '--graph-type',
-    choices=['filled', 'unfilled'],
-    default='unfilled',
-    help='Specify the type of graph that should be created. The default graph type is "unfilled". '
-         'To see the difference between a filled and unfilled graph, check out the example graph files.'
-)
-parser.add_argument(
-    '-gop',
-    action='store_true',
-    help='Instead of plotting the bitrate every second, plot the bitrate of each GOP. '
-         'This plots GOP end time (x-axis, in seconds) against GOP bitrate (y-axis, kbps).' 
-)
-parser.add_argument(
-    '-se', '--show-entries', 
+    "-f",
+    "--file-path",
     type=str,
-    default='packet=pts_time,size',
-    help='Only applicable if --no-graph-mode is specified. '
-         'Use FFprobe\'s -show_entries option to specify what to output. Example: -se frame=key_frame,pkt_pts_time'
+    required=True,
+    help="Enter the path of the file that you want to analyse. "
+    "If the path contains a space, it must be surrounded in double quotes. "
+    'Example: -f "C:/Users/H/Desktop/my file.mp4"',
 )
 parser.add_argument(
-    '-ngm', '--no-graph-mode', 
-    action='store_true',
+    "-g",
+    "--graph-type",
+    choices=["filled", "unfilled"],
+    default="unfilled",
+    help='Specify the type of graph that should be created. The default graph type is "unfilled". '
+    "To see the difference between a filled and unfilled graph, check out the example graph files.",
+)
+parser.add_argument(
+    "-gop",
+    action="store_true",
+    help="Instead of plotting the bitrate every second, plot the bitrate of each GOP. "
+    "This plots GOP end time (x-axis, in seconds) against GOP bitrate (y-axis, Mbps).",
+)
+parser.add_argument(
+    "-se",
+    "--show-entries",
+    type=str,
+    default="packet=dts_time,size",
+    help="Only applicable if --no-graph-mode is specified. "
+    "Use FFprobe's -show_entries option to specify what to output. Example: -se frame=key_frame,pkt_pts_time",
+)
+parser.add_argument(
+    "-ngm",
+    "--no-graph-mode",
+    action="store_true",
     help='Enable "no graph mode" which simply writes the output of ffprobe to a .txt file. '
-         'You should also use the --show-entries argument to specify what information you want ffprobe to output.'
+    "You should also use the --show-entries argument to specify what information you want ffprobe to output.",
 )
 parser.add_argument(
-    '-s', '--stream-specifier',
-    type=str, 
-    help='Use FFmpeg stream specifier syntax to specify the audio/video stream that you want to analyse. '
-         'The defaults for audio and video files are a:0 and V:0, respectively. '
-         'Note that stream index starts at 0. '
-         'As an example, to target the 2nd audio stream, enter: --stream-specifier a:1'
+    "-s",
+    "--stream-specifier",
+    type=str,
+    help="Use FFmpeg stream specifier syntax to specify the audio/video stream that you want to analyse. "
+    "The defaults for audio and video files are a:0 and V:0, respectively. "
+    "Note that stream index starts at 0. "
+    "As an example, to target the 2nd audio stream, enter: --stream-specifier a:1",
 )
 
 args = parser.parse_args()
@@ -60,122 +65,154 @@ filename_without_ext = Path(args.file_path).stem
 
 # This command will information about file's first stream.
 cmd = [
-    'ffprobe', '-v', 'error', '-threads', str(os.cpu_count()), 
-    '-show_streams', '-select_streams', '0', args.file_path
+    "ffprobe",
+    "-v",
+    "error",
+    "-threads",
+    str(os.cpu_count()),
+    "-show_streams",
+    "-select_streams",
+    "0",
+    args.file_path,
 ]
 
 process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-first_stream = process.stdout.read().decode('utf-8').replace('\r', '').split('\n')
+first_stream = process.stdout.read().decode("utf-8").replace("\r", "").split("\n")
 
 if not args.stream_specifier:
-    if 'codec_type=video' in first_stream:
-        print('Video file detected. The video stream will be analysed.')
-        stream_specifier = 'V:0'
-    elif 'codec_type=subtitle' in first_stream:
+    if "codec_type=video" in first_stream:
+        print("Video file detected. The video stream will be analysed.")
+        stream_specifier = "V:0"
+    elif "codec_type=subtitle" in first_stream:
         print(
-            'It seems like you have specified a video file. The video stream will be analysed.\n'
-            'If this is not what you want, re-run this program using the -s argument '
-            'to manually specify the stream to analyse.'
+            "It seems like you have specified a video file. The video stream will be analysed.\n"
+            "If this is not what you want, re-run this program using the -s argument "
+            "to manually specify the stream to analyse."
         )
-        stream_specifier = 'V:0'
+        stream_specifier = "V:0"
     else:
-        stream_specifier = 'a:0'
-        print('It seems like you have specified an audio file. The first audio stream will be analysed.')
+        stream_specifier = "a:0"
+        print(
+            "It seems like you have specified an audio file. The first audio stream will be analysed."
+        )
 else:
     stream_specifier = args.stream_specifier
-    print(f'The bitrate of stream {args.stream_specifier} will be analysed.')
+    print(f"The bitrate of stream {args.stream_specifier} will be analysed.")
 
 file_duration = get_file_duration(args.file_path, stream_specifier)
 
-if 'V' in stream_specifier or 'v' in stream_specifier:
+if "V" in stream_specifier or "v" in stream_specifier:
     number_of_frames = calc_number_of_frames(args.file_path, stream_specifier, file_duration)
 
 # To calculate the bitrate every second, FFprobe needs to output the following entries.
-entries = 'packet=pts_time,size'
+entries = "packet=dts_time,size"
 
 if args.no_graph_mode:
     entries = args.show_entries
 elif args.gop:
-    entries = 'frame=key_frame,pkt_pts_time,pkt_size'
+    entries = "frame=key_frame,pkt_dts_time,pkt_size"
 
 # The FFprobe command that will output the timestamps and packet sizes in CSV format.
 cmd = [
-    'ffprobe', '-v', 'error', '-threads', str(os.cpu_count()),
-    '-select_streams', stream_specifier, 
-    '-show_entries', entries, '-of', 'csv=print_section=0:nk=0',
-    args.file_path
+    "ffprobe",
+    "-v",
+    "error",
+    "-threads",
+    str(os.cpu_count()),
+    "-select_streams",
+    stream_specifier,
+    "-show_entries",
+    entries,
+    "-of",
+    "csv=print_section=0:nk=1",
+    args.file_path,
 ]
 process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
 
 if args.gop:
-    data_output_path = f'[{filename}]/Keyframes & GOPs.txt'
-    os.makedirs(f'[{filename}]', exist_ok=True)
-    with open(data_output_path, 'w'): pass
+    data_output_path = f"[{filename}]/Keyframes & GOPs.txt"
+    os.makedirs(f"[{filename}]", exist_ok=True)
+    with open(data_output_path, "w"):
+        pass
 
     gop_end_times, gop_bitrates = get_gop_bitrates(process, number_of_frames, data_output_path)
 
     plt.suptitle(filename)
-    plt.xlabel('GOP end time (s)')
-    plt.ylabel('GOP bitrate (kbps)')
+    plt.xlabel("GOP end time (s)")
+    plt.ylabel("GOP bitrate (Mbps)")
 
-    if args.graph_type == 'filled':
+    if args.graph_type == "filled":
         plt.fill_between(gop_end_times, gop_bitrates)
 
     plt.stem(gop_end_times, gop_bitrates)
     # Use mplcursors to show the X and Y value when hovering over a point on the line.
     cursor = mplcursors.cursor(hover=True)
     cursor.connect(
-        "add", lambda sel: sel.annotation.set_text(f'{round(sel.target[0], 1)}, {round(sel.target[1], 1)}')
+        "add",
+        lambda sel: sel.annotation.set_text(
+            f"{round(sel.target[0], 1)}, {round(sel.target[1], 1)}"
+        ),
     )
-    print('Done! The graph will open in a new window.')
+    print("Done! The graph will open in a new window.")
     plt.show()
 
 else:
     if args.no_graph_mode:
-        ffprobe_output_path = f'{filename} (FFprobe Data)/{entries}.txt'
-        os.makedirs(f'{filename} (FFprobe Data)', exist_ok=True)
+        ffprobe_output_path = f"{filename} (FFprobe Data)/{entries}.txt"
+        os.makedirs(f"{filename} (FFprobe Data)", exist_ok=True)
         frame_count = 0
+        # GOP length in terms of number of frames.
         gop_length = 0
-        print('-------------------------------------------------------------------------------------------------------')
-        print(f'{args.show_entries} data is being written to /{ffprobe_output_path}...')
+        print("-----------------------------------------------------------------------------------")
+        print(f"{args.show_entries} data is being written to /{ffprobe_output_path}...")
 
-        if 'key_frame' in args.show_entries or 'pict_type' in args.show_entries:
+        if "key_frame" in args.show_entries or "pict_type" in args.show_entries:
             for line in io.TextIOWrapper(process.stdout):
                 frame_count += 1
                 percentage_progress = round((frame_count / number_of_frames) * 100, 1)
-                print(f'Progress: {percentage_progress}%', end='\r')
+                print(f"Progress: {percentage_progress}%", end="\r")
                 gop_length += 1
-                # When splitting the CSV output, if one o 
-                if '1' in line.strip().split(',') or 'I' in line.strip().split(',') or \
-                'Iside_data' in line.strip().split(','):
-                    print('-------------------------------------------------------------------------------------------')
-                    print(f'Frame {frame_count} is an I-frame')
+                if (
+                    "1" in line.strip().split(",")
+                    or "I" in line.strip().split(",")
+                    or "Iside_data" in line.strip().split(",")
+                ):
+                    print("-----------------------------------------------------------------------")
+                    print(f"Frame {frame_count} is an I-frame")
+
                     if gop_length != 1:
-                        print(f'GOP length was {gop_length} frames')  
+                        print(f"GOP length was {gop_length} frames")
+
                     # We have reached the next keyframe, set gop_length to 0 to calculate the next GOP length.
-                    gop_length = 0 
+                    gop_length = 0
         else:
             for line in io.TextIOWrapper(process.stdout, encoding="utf-8"):
                 write_to_txt_file(ffprobe_output_path, line)
-        print(f'Done! Check out the following path: /{ffprobe_output_path}')
+        print(f"Done! Check out the following path: /{ffprobe_output_path}")
 
     else:
-        timestamp_bitrate_file = f'[{filename}]/BitrateEverySecond.txt'
-        os.makedirs(f'[{filename}]', exist_ok=True)
-        with open(timestamp_bitrate_file, 'w'): pass
+        timestamp_bitrate_file = f"[{filename}]/BitrateEverySecond.txt"
+        os.makedirs(f"[{filename}]", exist_ok=True)
+        with open(timestamp_bitrate_file, "w"):
+            pass
         # Parse the ffprobe output save the timestamps and bitrates in the time_data and size_data lists, respectively.
-        x_axis_values, bitrate_every_second = get_bitrate_every_second(process, timestamp_bitrate_file, file_duration)
+        x_axis_values, bitrate_every_second = get_bitrate_every_second(
+            process, timestamp_bitrate_file, file_duration
+        )
 
-        min = round(min(bitrate_every_second), 1)
-        max = round(max(bitrate_every_second), 1)
-        write_to_txt_file(timestamp_bitrate_file, f'\nMin Bitrate: {min} kbps\nMax Bitrate: {max} kbps')
+        min_bitrate = round(min(bitrate_every_second), 1)
+        max_bitrate = round(max(bitrate_every_second), 1)
+        write_to_txt_file(
+            timestamp_bitrate_file,
+            f"\nMin Bitrate: {min_bitrate} Mbps\nMax Bitrate: {max_bitrate} Mbps",
+        )
 
-        print('Creating the graph...')
+        print("Creating the graph...")
         plt.suptitle(filename)
-        plt.xlabel('Time (s)')
-        plt.ylabel('Bitrate (kbps)')
-        if args.graph_type == 'filled':
+        plt.xlabel("Time (s)")
+        plt.ylabel("Bitrate (Mbps)")
+        if args.graph_type == "filled":
             plt.fill_between(x_axis_values, bitrate_every_second)
         plt.plot(x_axis_values, bitrate_every_second)
-        print('Done! The graph will open in a new window.')
+        print("Done! The graph will open in a new window.")
         plt.show()
